4393581371347f11afd9618f388f08e37298daef
LMX04731 <143606185+LMX04731@users.noreply.github.com>
2023-08-31 18:08:16 +0800

    Add files via upload
    学习C语言的第二天

---
+++ 2day.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%zu\n",sizeof(char));
+	printf("%zu\n",sizeof(short));
+	printf("%zu\n", sizeof(int));
+	printf("%zu\n", sizeof(long));
+	printf("%zu\n",sizeof(long long));
+	printf("%zu\n",sizeof(float));
+	printf("%zu\n",sizeof(double));
+	return 0;
+}
